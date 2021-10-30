8a129e55e63bf0ee122e3f07c034d2c69363bd8d
Enrico <enricowakiman9@gmail.com>
2021-10-31 02:21:23 +0700

    Tengah malam mengerjakan endless recycler view, terakhir tinggal mengerjakan toolbar back button

--- app/src/main/java/com/enricowakiman/moviedb/view/fragments/MovieDetailsFragment.java
+++ app/src/main/java/com/enricowakiman/moviedb/view/fragments/MovieDetailsFragment.java
@@ -6,14 +6,12 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
